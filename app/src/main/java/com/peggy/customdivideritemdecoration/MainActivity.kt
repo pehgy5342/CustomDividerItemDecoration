786da47668e7f2032f27261ce0a367ea2343e5e5
Peggy <pehgy5342z@gmail.com>
2021-10-28 12:24:59 +0800

    CustomDividerItemDecoration

--- app/src/main/java/com/peggy/customdivideritemdecoration/MainActivity.kt
+++ app/src/main/java/com/peggy/customdivideritemdecoration/MainActivity.kt
@@ -2,6 +2,7 @@ package com.peggy.customdivideritemdecoration
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -11,31 +12,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val  recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
         fun getInfoList(): MutableList<Info> {
             val datas = mutableListOf<Info>(
-
                 Info("標題一", "123456789"),
                 Info("標題二", "987654321"),
                 Info("標題三", "987654321"),
                 Info("標題四", "123456789")
-
-
             )
             return datas
         }
 
-
-
-        val dividerItemDecoration = CustomDividerItemDecoration(resources.getDrawable(R.drawable.shape_line),60)
-        /**
-         * 第二种分割线 自定义 divider_bg
-         */
-        //  dividerItemDecoration.setDrawable(resources.getDrawable(R.drawable.shape_line))
-
-        val  recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-
+         /*自行定義樣式(shape -> 顏色跟高度)與尺寸(整個手機版面) */
+        val dividerItemDecoration = CustomDividerItemDecoration(resources.getDrawable(R.drawable.shape_line),resources.getDimensionPixelOffset(R.dimen.margin_30))
         recyclerView.addItemDecoration(dividerItemDecoration)
 
         val myadapter = MainAdapter(this,getInfoList())
